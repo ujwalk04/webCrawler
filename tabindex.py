2f20bf117d5b518de28e5b6ba4c0edc6698ef1b5
Ujwal Kumar <ujwalkumar4303@gmail.com>
2021-12-03 17:11:32 +0530

    Update tabindex.py

--- tabindex.py
+++ tabindex.py
@@ -5,12 +5,11 @@ html = requests.get(url).content
 c=0
 soup = BeautifulSoup(html, "html.parser")
 for tag in soup.find_all():
-    #print(tag)
+    
     try:
         if len(tag['tabindex']) >0:
-            #print("%%")
             attr=tag['tabindex']
-            #print(type(attr))
+            
             if int(attr)==0:
                 c=1
                 print("Null Tabindex found at :")
